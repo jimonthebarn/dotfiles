c82e99e3213f944c5bd99b4626a6456223f13539
Benjamin Roth <broth@telekom.de>
2017-09-08 13:27:15 +0200

    added alias: random_alias

--- .alias/general.sh
+++ .alias/general.sh
@@ -4,6 +4,7 @@ alias src="source ~/.zshrc"
 alias mkdir="mkdir -pv"
 alias shuf="gshuf"
 
-alias cowalias="alias | shuf -n 1 | cowthink | lolcat"
+alias random_alias="alias | shuf -n 1"
+alias cowalias="random_alias | cowthink | lolcat"
 alias ll="ls -lhA"
 alias cd..="cd .."
