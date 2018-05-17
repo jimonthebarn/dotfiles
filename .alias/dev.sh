bdf9da6761171f69445d1e652f7b09e3ee8e6673
Benjamin Roth <broth@telekom.de>
2018-05-17 09:40:21 +0200

    added setJdk function. fixed  autojump not working.

---
+++ .alias/dev.sh
@@ -0,0 +1,4 @@
+alias javaVersions="/usr/libexec/java_home -V"
+alias j8="export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Contents/Home; java -version"
+alias j9="export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home; java -version"
+alias j10="export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-10.0.1.jdk/Contents/Home; java -version"
