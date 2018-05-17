966a862b15508a827b8df948655e191eb7f4aaf9
Benjamin Roth <broth@telekom.de>
2018-05-17 09:41:31 +0200

    added setJdk function

--- .functions/functions.sh
+++ .functions/functions.sh
@@ -58,3 +58,14 @@ randomAlias() {
 
     echo "$aliases_all" | sed -n "$alias_index_selected"p
 }
+
+setJdk() {
+    if [[ $# -eq 1 ]]; then
+        export JAVA_HOME=$(/usr/libexec/java_home --version $1)
+    else
+        echo "Usage: \t\tsetJdk <javaVersion>"
+        echo "Example: \tsetJdk 1.8"
+        echo  "Available versions are:"
+        /usr/libexec/java_home --verbose
+    fi
+}
