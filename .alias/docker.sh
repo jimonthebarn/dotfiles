e543e68ebe0264ea26364b8d3628ba271ae6e4b9
Benjamin Roth <broth@telekom.de>
2018-06-06 13:25:41 +0200

    Added alias for running a dockerized elastic instance

--- .alias/docker.sh
+++ .alias/docker.sh
@@ -1,1 +1,2 @@
 alias elastic-core="docker run -p 9200:9200 -p 9300:9300 -e discovery.type=single-node -e cluster.name=elasticsearch -e xpack.security.enabled=false docker.elastic.co/elasticsearch/elasticsearch:5.6.0"
+alias elastic="docker run -p 9200:9200 -p 9300:9300 -e discovery.type=single-node -e cluster.name=elasticsearch docker.elastic.co/elasticsearch/elasticsearch:6.2.4"
