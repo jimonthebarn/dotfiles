477f2628b5177090d5ea50b1a27cc81e2b0c35c8
Benjamin Roth <roth.benjamin@gmail.com>
2018-06-13 21:00:53 +0200

    Resolved merge conflict by incorporating both suggestions.

--- .alias/dev.sh
+++ .alias/dev.sh
@@ -3,4 +3,3 @@ alias j8="export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Co
 alias j9="export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home; java -version"
 alias j10="export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-10.0.1.jdk/Contents/Home; java -version"
 alias elastic="docker run -p 9200:9200 -p 9300:9300 -e discovery.type=single-node docker.elastic.co/elasticsearch/elasticsearch:6.2.4"
-
