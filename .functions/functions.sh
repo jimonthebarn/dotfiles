c33d4448d1894343ffe9525b4a39818c85a49b1d
Benjamin Roth <broth@telekom.de>
2018-08-09 14:31:18 +0200

    Fixed usage hint

--- .functions/functions.sh
+++ .functions/functions.sh
@@ -146,7 +146,7 @@ pofo() {
     if [[ $# -eq 1 ]]; then
         oc get pods | grep $1 | grep Running | sed 's/^\([^ ][^ ]*\) .*/oc port-forward \1 8080/'
     else
-        echo "Usage: \t\pofo <podname>"
+        echo "Usage: \t\tpofo <podname>"
         echo "Example: \tpofo canned"
     fi
 }
