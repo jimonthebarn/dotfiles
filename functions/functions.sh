d012a7918f3b6bbcce4570103908dfc8d11ba132
Benjamin Roth <broth@telekom.de>
2018-08-28 15:59:52 +0200

    Fixed create alias function.

--- functions/functions.sh
+++ functions/functions.sh
@@ -4,10 +4,10 @@ createAlias() {
             echo "Alias $2 already exists."
         else
             echo "alias $2=\"$3\"" >> ~/dotfiles/alias/$1.sh
-            source ~/.alias/$1.sh
+            source ~/dotfiles/alias/$1.sh
         fi
     elif [[ $# -eq 2 ]]; then
-        createAlias general $1 $2
+        createAlias general "$1 $2"
     else
         echo "Usage: \tcreateAlias <fileNameWithoutExtension> <aliasName> <alias>"
         echo "Example: \tcreateAlias npm nis \"npm install -save\""
