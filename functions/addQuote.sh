aae47ea6e44ae2afdab33b6ee944662b5ce90524
Benjamin Roth <broth@telekom.de>
2019-09-16 14:18:20 +0200

    Added quote alias.

--- functions/addQuote.sh
+++ functions/addQuote.sh
@@ -1,6 +1,6 @@
 addQuote() {
     if [[ $# -eq 1 ]]; then
-        echo "$1" >> ~/dotfiles/quotes.txt
+        echo "$1" >> ~/dotfiles/misc/quotes.txt
     else
         echo "Usage: \taddQuote '<your quote>'"
         echo "Example: \taddQuote 'Dress for the job you want, not the job you have.'"
