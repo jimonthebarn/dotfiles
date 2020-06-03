16467816b0dc3f2c8d44c45c8e285aeb06703e3c
Benjamin Roth <broth@telekom.de>
2020-06-03 13:32:57 +0200

    Added additional installations to debian setup for lolcat and pyenv.

--- .zshrc
+++ .zshrc
@@ -28,7 +28,7 @@ zstyle :omz:plugins:ssh-agent agent-forwarding on
 export TERM="xterm-256color"
 
 # If you come from bash you might have to change your $PATH.
-export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$HOME/AppLauncher:$PATH
+export PATH=$HOME/.pyenv/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$HOME/AppLauncher:$PATH
 
 # Path to your oh-my-zsh installation.
 export ZSH="${HOME}/.oh-my-zsh"
--- debian/setup.sh
+++ debian/setup.sh
@@ -1,11 +1,11 @@
-#install node.js
-sudo apt-get update
-sudo apt-get install git-core curl build-essential openssl libssl-dev python
-git clone https://github.com/nodejs/node.git
-cd node
-./configure
-make
-sudo make install
+#install node.js: do manually if required (compiling takes ages)
+#sudo apt-get update
+#sudo apt-get install git-core curl build-essential openssl libssl-dev python
+#git clone https://github.com/nodejs/node.git
+#cd node
+#./configure
+#make
+#sudo make install
 
 #install npm
 curl -L https://npmjs.org/install.sh | sudo zsh
@@ -18,3 +18,17 @@ sudo pip3 install thefuck
 #install cowsay
 sudo apt-get update -y
 sudo apt-get install -y cowsay
+
+#install pyenv
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | zsh
+
+#install lolcat
+apt-get install ruby
+wget https://github.com/busyloop/lolcat/archive/master.zip
+unzip master.zip
+cd lolcat-master/bin
+gem install lolcat
+#sudo snap install lolcat #if WSL supported snapd
