f48c5bd5032e73dd321176666d161f895c127850
Benjamin Roth <broth@telekom.de>
2020-06-03 12:32:27 +0200

    Added additional installations to debian setup for node.js and npm

--- debian/setup.sh
+++ debian/setup.sh
@@ -1,3 +1,15 @@
+#install node.js
+sudo apt-get update
+sudo apt-get install git-core curl build-essential openssl libssl-dev python
+git clone https://github.com/nodejs/node.git
+cd node
+./configure
+make
+sudo make install
+
+#install npm
+curl -L https://npmjs.org/install.sh | sudo zsh
+
 #install thefuck
 sudo apt update
 sudo apt install python3-dev python3-pip python3-setuptools
