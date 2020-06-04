e6d8407ad468bc1082cd1234b1b3bf22e376c724
Benjamin Roth <broth@telekom.de>
2020-06-04 11:10:14 +0200

    Renamed docker aliases.

--- alias/docker.sh
+++ alias/docker.sh
@@ -1,8 +1,9 @@
-alias elastic-core="docker run -p 9200:9200 -p 9300:9300 -e discovery.type=single-node -e cluster.name=elasticsearch -e xpack.security.enabled=false docker.elastic.co/elasticsearch/elasticsearch:5.6.0"
-alias elastic="docker run -p 9200:9200 -p 9300:9300 -e discovery.type=single-node -e cluster.name=elasticsearch docker.elastic.co/elasticsearch/elasticsearch:6.2.4"
-alias browsh="docker run -it browsh/browsh"
+#alias docker-elastic="docker run -p 9200:9200 -p 9300:9300 -e discovery.type=single-node -e cluster.name=elasticsearch -e xpack.security.enabled=false docker.elastic.co/elasticsearch/elasticsearch:5.6.0"
+alias docker-redis="docker run -p 6379:6379 --rm --name container-redis -d redis"
+alias docker-elastic="docker run -p 9200:9200 -p 9300:9300 -e discovery.type=single-node -e cluster.name=elasticsearch docker.elastic.co/elasticsearch/elasticsearch:6.2.4"
+alias docker-browsh="docker run -it browsh/browsh"
 alias dkilla='docker kill $(docker ps -q)'
 alias drma='docker rm $(docker ps -a -q)'
 alias dlastc='docker ps -l --format "{{.ID}}"'
 alias dlogl='docker logs -f $(dlastc)'
-alias httpbin='docker run -p 80:80 kennethreitz/httpbin'
+alias docker-httpbin='docker run --rm -p 80:80 kennethreitz/httpbin'
