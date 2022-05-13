c248fa1fc0965bed9195665dfbd7569a7ce6852c
Benjamin Roth <roth.benjamin@gmail.com>
2022-05-13 14:10:28 +0200

    Allow quotations to be attributed

--- functions/addQuote.sh
+++ functions/addQuote.sh
@@ -1,8 +1,10 @@
 addQuote() {
     if [[ $# -eq 1 ]]; then
-        echo "$1" >> ~/dotfiles/misc/quotes.txt
+        echo "'$1' by 'unknown'\r" >> ~/dotfiles/misc/quotes.txt
+    elif [[ $# -eq 2 ]]; then
+        echo "'$1' by '$2'\r" >> ~/dotfiles/misc/quotes.txt
     else
-        echo "Usage: \taddQuote '<your quote>'"
+        echo "Usage: \taddQuote '<your quote>' ''"
         echo "Example: \taddQuote 'Dress for the job you want, not the job you have.'"
         return 1
     fi
