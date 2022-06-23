4c9bf33efb59d552492091ba310cd35611743450
Benjamin Roth <roth.benjamin@gmail.com>
2022-06-23 15:12:07 +0200

    Add alias cd..

--- alias/general.sh
+++ alias/general.sh
@@ -4,5 +4,5 @@ alias random_alias="alias | shuf -n 1 | cowthink | lolcat"
 alias norris="chuck_cow | lolcat"
 alias hitch="hitchhiker_cow | lolcat"
 alias ll="ls -lha"
-#alias cd..="cd .."
+alias cd..="cd .."
 alias youtube-dl='youtube-dl -f best --no-cache-dir'
