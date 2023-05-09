f1faeb32460354f5ea6438c4f9e1c1b36ef9c23a
Benjamin Roth <roth.benjamin@gmail.com>
2023-05-09 12:09:59 +0200

    Add gitprune alias

--- alias/git.sh
+++ alias/git.sh
@@ -2,3 +2,4 @@ alias gpom="git push origin master"
 #alias git='hub'
 alias glfm='git log origin/master ^master'
 alias gitkill='git filter-repo --invert-paths --path '
+alias gitprune='git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -d'
