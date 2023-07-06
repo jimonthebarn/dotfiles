ff4565277d92df04f97d0ae480ccbdbb29af0df0
Benjamin Roth <roth.benjamin@gmail.com>
2023-07-06 11:00:57 +0200

    Combine postgres shuttle command

--- alias/work.sh
+++ alias/work.sh
@@ -1,2 +1,3 @@
+alias svhpg="jmpstag && shuttlepg"
 alias jmpstag="az ssh config --ip jump-staging.smartvoicehub.de --file ./jump-config"
 alias shuttlepg="sshuttle -r jump-staging.smartvoicehub.de -e \"ssh -F ./jump-config -p 1077\" --dns 10.0.0.0/8:5432"
